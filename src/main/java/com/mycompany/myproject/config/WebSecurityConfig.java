07a9a2001fce5d464e27ef5025491e69e5e453aa
minjiKim87 <68892132+minjiKim87@users.noreply.github.com>
2023-10-03 20:57:43 +0900

    Add files via upload

--- src/main/java/com/mycompany/myproject/config/WebSecurityConfig.java
+++ src/main/java/com/mycompany/myproject/config/WebSecurityConfig.java
@@ -2,15 +2,15 @@ package com.mycompany.myproject.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
+public class WebSecurityConfig  {
+
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests()
+                . authorizeHttpRequests()
 //                .anyRequest().authenticated()
 //                .and()
 //                .formLogin().permitAll();
--- src/main/java/com/mycompany/myproject/domain/posts/RecordContent.java
+++ src/main/java/com/mycompany/myproject/domain/posts/RecordContent.java
@@ -4,7 +4,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+
+import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
