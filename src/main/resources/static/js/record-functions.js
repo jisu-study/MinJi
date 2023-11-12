c9ee3e0000337a8e52214b295f514af0a40dcad4
minjiKim87 <68892132+minjiKim87@users.noreply.github.com>
2023-11-12 11:28:36 +0900

    Add files via upload

--- src/main/resources/static/js/record-functions.js
+++ src/main/resources/static/js/record-functions.js
@@ -1,36 +1,26 @@
+var tempData = {
+    recordTitle: null,
+    location: null,
+    startDate: null,
+    endDate: null,
+};
 
-  ///기존 데이터 보여주는 용도///
-//  $(document).ready(function() {
-//        $.get("/fetch-records", function(data) {
-//    data.forEach(record => {
-//        let startDate = new Date(record.startDate).toISOString().split("T")[0];
-//        let endDate = new Date(record.endDate).toISOString().split("T")[0];
-//
-//        $('#recordList').append(`
-//            <tr data-id="${record.recordId}">
-//                <td class="recordTitle">${record.recordTitle}</td>
-//                <td class="location">${record.location}</td>
-//                <td class="startDate">${startDate}</td>
-//                <td class="endDate">${endDate}</td>
-//                <td>
-//                    <button onclick="updateRecord(${record.recordId})">업데이트</button>
-//                    <button onclick="deleteRecord(${record.recordId})">삭제</button>
-//                </td>
-//            </tr>
-//        `);
-//    });
-//});
-
-///
+function saveTempData() {
+    tempData.recordTitle = $("#recordTitle").val();
+    tempData.location = $("#location").val();
+    tempData.startDate = $("#startDate").val();
+    tempData.endDate = $("#endDate").val();
+    alert("임시 저장 완료!");
+}
 
 function handleFormSubmit() {
 
-        let data = {
-            recordTitle: $("#recordTitle").val(),
-            location: $("#location").val(),
-            startDate: $("#startDate").val(),
-            endDate: $("#endDate").val()
-        };
+   if (Object.values(tempData).some(item => item === null)) {
+        alert("모든 필드를 채워주세요.");
+        return;
+    }
+
+         let data = tempData;
 
         $.ajax({
             headers: {
@@ -47,6 +37,8 @@ function handleFormSubmit() {
                 //date_content.js에 recordId 넘기는 용도
                 recordId = response;
                 alert("Received recordId from the server: " + response);
+                window.location.href = `http://localhost:8080/view_detail_record?recordId=${recordId}`;
+
 
 
             },
@@ -96,6 +88,7 @@ window.saveRecord = function(recordId) {
                 contentType: "application/json; charset=utf-8",
                 dataType: "json",
                 success: function(response) {
+                    alert(JSON.stringify(response));
                     alert("성공적으로 수정되었습니다!");
                     recordTitleInput.parent().text(data.recordTitle);
                     locationInput.parent().text(data.location);
@@ -128,5 +121,39 @@ window.deleteRecord = function(recordId) {
             }
         }
 
+function generateContentsFields(recordId) {
+    var startDate = tempData.startDate;
+    var endDate = tempData.endDate;
+
+    // 날짜 차이 계산
+    var start = new Date(startDate);
+    var end = new Date(endDate);
+    var timeDiff = Math.abs(end.getTime() - start.getTime());
+    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); // 차이 일 수 계산
+
+    // 텍스트 칸 동적 생성
+    var templateContainer = document.createElement("div");
+    for (var i = 0; i <= diffDays; i++) {
+        var dateSpan = document.createElement("span");
+        dateSpan.textContent = startDate;
+
+        var textField = document.createElement("input");
+        textField.setAttribute("type", "text");
+        textField.setAttribute("placeholder", startDate);
+
+        templateContainer.appendChild(dateSpan);
+        templateContainer.appendChild(textField);
+        templateContainer.appendChild(document.createElement("br")); // 줄 바꿈 추가
+        startDate = getNextDay(startDate); // 다음 날짜 계산
+    }
+
+    var targetDiv = document.querySelector(".content-space");
+    targetDiv.appendChild(templateContainer);
+}
 
 
+function getNextDay(dateString) {
+    var date = new Date(dateString);
+    date.setDate(date.getDate() + 1);
+    return date.toISOString().split("T")[0];
+}
